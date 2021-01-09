ca8adddb36bc45be018c8b67b34b35d7afd57060
Shawn Santhoshgeorge <shawngeorge2001@gmail.com>
2021-01-09 00:13:27 -0500

    Added a better command handler

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
-client.once('ready', () => {
-    console.log('Translator is online');
+const prefix = "!";
+
+const fs = require("fs");
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs
+  .readdirSync("./commands/")
+  .filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+
+  client.commands.set(command.name, command);
+}
+
+client.once("ready", () => {
+  console.log("Translator is online");
+});
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === "ping") {
+    client.commands.get('ping').execute(message, args);
+}
 });
 
 client.login(process.env.TOKEN);
